c8abd77466e39a493dd4a7429d50e26fc8cb6713
Dennis Sergeev <dennis.sergeev@gmail.com>
2021-01-21 11:08:20 +0000

    Update cloudsat_read module to fix deprecated property in h5py; update environment file and apply black formatting

--- utils.py
+++ utils.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Auxiliary functions for sattools module
-"""
+"""Auxiliary functions for sattools module."""
 import numpy as np
 
 
@@ -10,7 +8,7 @@ def cc_interp2d(data, X, Z, x1, x2, nx, z1, z2, nz, use_numba=True):
         try:
             import numba as nb
         except ImportError:
-            print('Unsuccessful numba import, using pure Python')
+            print("Unsuccessful numba import, using pure Python")
             use_numba = False
 
     if use_numba:
@@ -32,8 +30,8 @@ def _interp2d(data, X, Z, x1, x2, nx, z1, z2, nz):
     with no mask and no transformation (such as transposition) applied.
     """
 
-    xs = (x2 - x1)/nx
-    zs = (z2 - z1)/nz
+    xs = (x2 - x1) / nx
+    zs = (z2 - z1) / nz
     w = data.shape[0]
     h = data.shape[1]
 
@@ -41,19 +39,19 @@ def _interp2d(data, X, Z, x1, x2, nx, z1, z2, nz):
     q = np.zeros((nx, nz), dtype=np.int32)
 
     for i in range(w):
-        n1 = ((X[i-1] + X[i])/2 - x1)/xs if i-1 >= 0 else -1
-        n2 = ((X[i+1] + X[i])/2 - x1)/xs if i+1 < w else nx
+        n1 = ((X[i - 1] + X[i]) / 2 - x1) / xs if i - 1 >= 0 else -1
+        n2 = ((X[i + 1] + X[i]) / 2 - x1) / xs if i + 1 < w else nx
         if n2 - n1 < 1:
-            n1 = n2 = (X[i] - x1)/xs
+            n1 = n2 = (X[i] - x1) / xs
 
         for j in range(h):
-            m1 = ((Z[i, j-1] + Z[i, j])/2 - z1)/zs if j-1 >= 0 else -1
-            m2 = ((Z[i, j+1] + Z[i, j])/2 - z1)/zs if j+1 < h else nz
+            m1 = ((Z[i, j - 1] + Z[i, j]) / 2 - z1) / zs if j - 1 >= 0 else -1
+            m2 = ((Z[i, j + 1] + Z[i, j]) / 2 - z1) / zs if j + 1 < h else nz
             if m2 - m1 < 1:
-                m1 = m2 = (Z[i, j] - z1)/zs
+                m1 = m2 = (Z[i, j] - z1) / zs
 
-            for n in range(int(n1+0.5), int(n2+0.5+1)):
-                for m in range(int(m1+0.5), int(m2+0.5+1)):
+            for n in range(int(n1 + 0.5), int(n2 + 0.5 + 1)):
+                for m in range(int(m1 + 0.5), int(m2 + 0.5 + 1)):
                     if n < 0 or n >= nx or m < 0 or m >= nz:
                         continue
                     if np.isnan(data[i, j]):
